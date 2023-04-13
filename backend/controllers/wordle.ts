81fc5a605f26dce3d933571773dd047ae6b24fd3
Eivind Dalholt <eividalh@stud.ntnu.no>
2023-04-13 10:00:36 +0200

    fix: duplicate letter response fix

--- backend/controllers/wordle.ts
+++ backend/controllers/wordle.ts
@@ -20,17 +20,28 @@ export const guessWord = async (req: Request, res: Response) => {
   }
 
   const word = (await Word.find().skip(wordIndex).limit(1))[0].word;
+  let wordWithoutCorrect = word;
 
   const result = [];
 
   for (let i = 0; i < word.length; i++) {
     if (word[i] == guess[i]) {
+      // Remove correct guesses to avoid wrong feedback when there is words with more instances of the same letter.
+      // New word without correct guesses.
+      for (let x = 0; x < wordWithoutCorrect.length; x++) {
+        if (wordWithoutCorrect[x] == word[i]) {
+          wordWithoutCorrect =
+            wordWithoutCorrect.slice(0, x) + wordWithoutCorrect.slice(x + 1);
+          break;
+        }
+      }
+
       result.push({
         letter: guess[i],
         status: 1,
         placement: 1,
       });
-    } else if (word.includes(guess[i])) {
+    } else if (wordWithoutCorrect.includes(guess[i])) {
       result.push({ letter: guess[i], status: 1, placement: 0 });
     } else {
       result.push({ letter: guess[i], status: 0, placement: 0 });
