75119cf9abac0a7ad5dd1bd9cc96dce017d7b4b6
Jakob Keller <jekeller@stud.ntnu.no>
2023-04-22 20:23:13 +0200

    singleton player (#34)

--- frontend/core/src/com/wordle/royale/v2/model/Player.java
+++ frontend/core/src/com/wordle/royale/v2/model/Player.java
@@ -2,9 +2,23 @@ package com.wordle.royale.v2.model;
 
 public class Player extends User {
 
-    public Player(String userName) {
-        super(userName);
+    private static Player player = null;
+    private Player() {
+        super();
     }
+
+    public static Player getInstance() {
+        if (player == null) {
+            player = new Player();
+        }
+        return  player;
+    }
+
+    public Player setName(String name) {
+        player.userName = name;
+        return player;
+    }
+
 
 }
 
