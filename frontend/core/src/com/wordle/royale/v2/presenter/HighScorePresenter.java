ebed80f3f5a9a2befe866291bedc0043247634bc
Eivind Dalholt <eividalh@stud.ntnu.no>
2023-04-22 19:24:21 +0200

    highscore screen with real data

--- frontend/core/src/com/wordle/royale/v2/presenter/HighScorePresenter.java
+++ frontend/core/src/com/wordle/royale/v2/presenter/HighScorePresenter.java
@@ -1,19 +1,46 @@
 package com.wordle.royale.v2.presenter;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.wordle.royale.v2.model.network.ScoreApiService;
+import com.wordle.royale.v2.model.other.HighScore;
 import com.wordle.royale.v2.model.other.ScreenController;
 
-public class HighScorePresenter {
-    private ScreenController parent;
 
+public class HighScorePresenter {
+    private ScoreApiService ScoreAPI;
+    private ScreenController parent;
+    HighScore highScore;
+
+
+    public HighScore getHighScore() {
+        return highScore;
+    }
 
     public HighScorePresenter(ScreenController parent) {
         this.parent = parent;
+        highScore = new HighScore();
+
+        ScoreAPI = new ScoreApiService();
+        ScoreAPI.getHigscores(new ScoreApiService.CallbackPostScore<com.wordle.royale.v2.model.other.HighScore>() {
+            @Override
+            public void onSuccess(com.wordle.royale.v2.model.other.HighScore highscores) {
+                highScore = highscores;
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println("Failed to fetch scores");
+                highScore = new HighScore();
+                highScore.addHighscore("Network error", 0);
+            }
+        });
     }
 
     public void changeScreensFunc(int i) {
         parent.changeScreens(i);
     }
-
 
     public interface changeScreens {
         void setupChangeToMenu();
--- frontend/core/src/com/wordle/royale/v2/view/screens/HighScoreScreen.java
+++ frontend/core/src/com/wordle/royale/v2/view/screens/HighScoreScreen.java
@@ -30,9 +30,9 @@ public class HighScoreScreen implements Screen, HighScorePresenter.changeScreens
     private TextButton backToMenu;
     private TextButton playAgain;
     private ScreenController parent;
-    private HighScore highScore;
     private BitmapFont title;
     private GlyphLayout layout;
+    private BitmapFont font;
     private HighScorePresenter presenter;
 
     public HighScoreScreen(ScreenController parent) {
@@ -41,6 +41,8 @@ public class HighScoreScreen implements Screen, HighScorePresenter.changeScreens
 
     @Override
     public void show() {
+        font = new BitmapFont();
+        font.getData().setScale(2f, 4f);
         title = new BitmapFont(Gdx.files.internal("craftacular/raw/font-title-export.fnt"));
         title.getData().setScale(.7f, 2f);
         layout = new GlyphLayout(title, "Top 5 Legends");
@@ -72,12 +74,19 @@ public class HighScoreScreen implements Screen, HighScorePresenter.changeScreens
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        highScore = new HighScore();
 
         batch.begin();
-        title.draw(batch, "Top 5 Legends", Gdx.graphics.getWidth()/2f - layout.width/2, Gdx.graphics.getHeight()- layout.height*2);
+        title.draw(batch, "Top 10 Legends", Gdx.graphics.getWidth()/2f - layout.width/2, Gdx.graphics.getHeight()- layout.height*2);
 
-        highScore.render(batch);
+        for (int i = 0; i < presenter.getHighScore().getHighscoreList().size(); i++) {
+            float xPos = Gdx.graphics.getWidth() / 2.5f;
+            float yPos = Gdx.graphics.getHeight() / 1.5f - (i * 75);
+            String name = presenter.getHighScore().getHighscoreList().get(i).getUsername();
+            Integer score = presenter.getHighScore().getHighscoreList().get(i).getScore();
+            String text = i + 1 + ". " + name + " : " + score + " points";
+            font.draw(batch, text, xPos, yPos);
+        }
+
         stage.draw();
 
         batch.end();
